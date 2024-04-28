75188d5c377514a9df7a0e09d7458e2446725393
Marly Mofeed <109438797+MarlyMofeed@users.noreply.github.com>
2024-04-28 20:50:24 +0300

    created: add documnt

--- Backend/googledocsclone/src/main/java/googledocsclone/example/googledocsclone/controllers/DocumentController.java
+++ Backend/googledocsclone/src/main/java/googledocsclone/example/googledocsclone/controllers/DocumentController.java
@@ -1,15 +1,38 @@
 package googledocsclone.example.googledocsclone.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import googledocsclone.example.googledocsclone.repositories.DocumentRepository;
+import googledocsclone.example.googledocsclone.models.Documents;
+
 
 @RestController
+@RequestMapping("/document")
 public class DocumentController {
-    @PostMapping("/document")
-    public String postMethodName(@RequestBody String entity) {
 
-        return entity;
+    @Autowired
+    DocumentRepository DocumentRepository;
+    @PostMapping("/add")
+    public String addDocument(@RequestBody Map<String, String> body) {
+        Documents document = new Documents();
+        document.setName(body.get("documentName"));
+        DocumentRepository.save(document);
+        return "Document added successfully";
     }
+    @DeleteMapping("/delete")
+    public String deleteDocument(@RequestParam String documentName) {
+        DocumentRepository.deleteByName(documentName);
+        return "Document deleted successfully";
+    }
+
 
 }
