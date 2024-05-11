e040ba5c3ba44211a5d911f6f8da82bcf5ada605
Marly Mofeed <109438797+MarlyMofeed@users.noreply.github.com>
2024-05-11 03:13:28 +0300

    adddd

--- texteditor/lib/views/textEdit.dart
+++ texteditor/lib/views/textEdit.dart
@@ -9,7 +9,6 @@ import 'dart:async';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-
 //pendingOperation=Null
 //changesBufferQueue = null initially
 //version=0 on start
@@ -38,7 +37,6 @@ import 'dart:convert';
 // then update el version
 // then for each element gowa el changes buffer brdo apply transformation 3shan lazm}
 
-
 //processMessage(replyFromServer)
 //{
 //if reply == pending -> hasal ack, hasheel el pending arg3o b null then version++
@@ -58,7 +56,6 @@ import 'dart:convert';
 // this means en insert a at 0 0 de heya version 0 of document
 // insert b at 0 1 de heya version 1 of document
 // delete at 0 0 de heya version 2 of document
-
 
 class TextEdit extends StatefulWidget {
   const TextEdit({
@@ -85,22 +82,25 @@ class _TextEditState extends State<TextEdit> {
   String userId = '6633742c11cf8763d7b4b5f3';
   String documentId = '6633ad108901bd48cf18bb60';
   List<String> content = ['your', 'document', 'content'];
-
+  int version = 0;
+  List<Map<String, int>> changesBuffer = [];
 
   @override
   void initState() {
     super.initState();
     _previousText = _controller.document.toPlainText();
+    //version = document.ge;
     _autosaveTimer = Timer.periodic(Duration(minutes: 1), (Timer t) {
-    _saveDocument(userId, documentId, _previousText);
-  });
+      _saveDocument(userId, documentId, _previousText);
+    });
 
     try {
-      channel = HtmlWebSocketChannel.connect('ws://localhost:8080/document/editContent/socket');
+      channel = HtmlWebSocketChannel.connect(
+          'ws://localhost:8080/document/editContent/socket');
       print('Connected to WebSocket server!');
-      channel.stream.listen((message) 
-      {
-        print('FRONTEND Received message: $message');
+      channel.stream.listen(
+        (message) {
+          print('FRONTEND Received message: $message');
         },
         onError: (error) {
           print('Error NOT listening to stream: $error');
@@ -109,30 +109,28 @@ class _TextEditState extends State<TextEdit> {
     } catch (e) {
       print('Error establishing WebSocket connection: $e');
     }
-    
+
     // Listen to the onChange event of the QuillController
     _controller.addListener(() {
       String currentText = _controller.document.toPlainText();
       if (currentText.length > _previousText.length) {
         int addedIndex = _findAddedIndex(_previousText, currentText);
         //print('Text added at index: $addedIndex');
-        row = addedIndex ~/171;
+        row = addedIndex ~/ 171;
         column = addedIndex % 171;
       } else if (currentText.length < _previousText.length) {
         int removedIndex = _findRemovedIndex(_previousText, currentText);
         //print('Text removed at index: $removedIndex');
-        row =  removedIndex~/171;
+        row = removedIndex ~/ 171;
         column = removedIndex % 171;
       }
       _previousText = currentText;
-      if(operation == 'Insert'){
+      if (operation == 'Insert') {
         channel.sink.add('Insert $element $row $column');
         //print('$operation $element in row: $row , column: $column');
-      }
-      else if(operation == 'Delete'){
+      } else if (operation == 'Delete') {
         channel.sink.add('Delete $row $column');
         //print('$operation from row: $row , column: $column');
-
       }
       //_cursorPosition = _controller.selection.baseOffset; // Update cursor position
     });
@@ -153,49 +151,46 @@ class _TextEditState extends State<TextEdit> {
   //   channel.sink.add('Save $currentText');
   // }
 
-void _saveDocument(String userId, String documentId, String content) async {
-  var url = Uri.parse('http://localhost:8080/document/save');
+  void _saveDocument(String userId, String documentId, String content) async {
+    var url = Uri.parse('http://localhost:8080/document/save');
 
-  var response = await http.put(url, 
-    headers: <String, String>{
-      'Content-Type': 'application/json; charset=UTF-8',
-      'userId': userId,
-    },
-    body: jsonEncode(<String, dynamic>{
-      'id': documentId,
-      'documentContent': content,
-    }),
-  );
+    var response = await http.put(
+      url,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'userId': userId,
+      },
+      body: jsonEncode(<String, dynamic>{
+        'id': documentId,
+        'documentContent': content,
+      }),
+    );
 
-  if (response.statusCode == 200) {
-    print('Document saved successfully');
-  } else {
-    print('Failed to save document');
+    if (response.statusCode == 200) {
+      print('Document saved successfully');
+    } else {
+      print('Failed to save document');
+    }
   }
-}
 
- void _handleKeyPress(RawKeyEvent event) {
-  if (event is RawKeyDownEvent) {
-    LogicalKeyboardKey logicalKey = event.logicalKey;
-    String? keyLabel = logicalKey.keyLabel ?? logicalKey.debugName;
-    //print('Key pressed: $keyLabel');
-    if(keyLabel == 'Backspace'){
-      operation = 'Delete';
+  void _handleKeyPress(RawKeyEvent event) {
+    if (event is RawKeyDownEvent) {
+      LogicalKeyboardKey logicalKey = event.logicalKey;
+      String? keyLabel = logicalKey.keyLabel ?? logicalKey.debugName;
+      //print('Key pressed: $keyLabel');
+      if (keyLabel == 'Backspace') {
+        operation = 'Delete';
+      } else if (keyLabel == 'Enter') {
+        operation = 'Insert';
+        element = 'New Line';
+        row++;
+      } else {
+        operation = 'Insert';
+        element = keyLabel;
+      }
+      //print('Cursor position: $_cursorPosition'); //not accurate awy aw msh fhma howa shaghal ezay?
     }
-    else if(keyLabel == 'Enter')
-    {
-      operation = 'Insert';
-      element = 'New Line';
-      row++;
-    }
-      else{
-      operation = 'Insert';
-      element = keyLabel;
-    }
-    //print('Cursor position: $_cursorPosition'); //not accurate awy aw msh fhma howa shaghal ezay?
   }
-}
-
 
   int _findAddedIndex(String previousText, String currentText) {
     for (int i = 0; i < currentText.length; i++) {
