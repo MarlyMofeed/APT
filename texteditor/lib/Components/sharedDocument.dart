dd2490000e315a314059b70811bcb763ccb64e36
Marly Mofeed <109438797+MarlyMofeed@users.noreply.github.com>
2024-05-14 02:36:04 +0300

    changed: stateful widget

--- texteditor/lib/Components/sharedDocument.dart
+++ texteditor/lib/Components/sharedDocument.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:texteditor/views/file_management.dart';
 
-class SharedDocuments extends StatelessWidget {
-  const SharedDocuments({
-    super.key,
-    required this.sharedDocuments,
-    required this.isEditor,
-  });
-
+class SharedDocuments extends StatefulWidget {
   final List<Document> sharedDocuments;
   final bool isEditor;
 
+  const SharedDocuments({
+    Key? key,
+    required this.sharedDocuments,
+    required this.isEditor,
+  }) : super(key: key);
+
+  @override
+  _SharedDocumentsState createState() => _SharedDocumentsState();
+}
+
+class _SharedDocumentsState extends State<SharedDocuments> {
   @override
   Widget build(BuildContext context) {
     return Expanded(
@@ -24,16 +29,14 @@ class SharedDocuments extends StatelessWidget {
             ),
             textAlign: TextAlign.left,
           ),
-          //),
           Padding(
             padding: const EdgeInsets.fromLTRB(150, 30, 150, 100),
             child: ListView.builder(
               shrinkWrap: true,
               physics: NeverScrollableScrollPhysics(),
-              itemCount: sharedDocuments.length,
+              itemCount: widget.sharedDocuments.length,
               itemBuilder: (context, index) {
-                final document = sharedDocuments[index];
-                //var isEditor;
+                final document = widget.sharedDocuments[index];
                 return Container(
                   margin: const EdgeInsets.only(bottom: 10),
                   decoration: BoxDecoration(
@@ -46,7 +49,7 @@ class SharedDocuments extends StatelessWidget {
                       // TODO: Handle opening the document
                     },
                     trailing: PopupMenuButton<String>(
-                      itemBuilder: (context) => isEditor
+                      itemBuilder: (context) => widget.isEditor
                           ? [
                               PopupMenuItem(
                                 value: 'Rename',
