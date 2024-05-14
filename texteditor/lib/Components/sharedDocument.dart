224b5fb002813b771e0e49526205ed2227332f83
Marly Mofeed <109438797+MarlyMofeed@users.noreply.github.com>
2024-05-14 05:23:51 +0300

    added: shared document widget integrated

--- texteditor/lib/Components/sharedDocument.dart
+++ texteditor/lib/Components/sharedDocument.dart
@@ -1,15 +1,21 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:texteditor/Components/shareDialog.dart';
+import 'package:texteditor/models/Document.dart';
 import 'package:texteditor/views/file_management.dart';
+import 'package:http/http.dart' as http;
+import 'package:texteditor/views/textEdit.dart';
+import 'package:texteditor/views/viewOnly.dart';
 
 class SharedDocuments extends StatefulWidget {
-  final List<Document> sharedDocuments;
   final bool isEditor;
+  final String userId;
 
   const SharedDocuments({
     Key? key,
-    required this.sharedDocuments,
     required this.isEditor,
+    required this.userId,
   }) : super(key: key);
 
   @override
@@ -17,8 +23,61 @@ class SharedDocuments extends StatefulWidget {
 }
 
 class _SharedDocumentsState extends State<SharedDocuments> {
+  final List<Document> editorDocuments = [];
+  final List<Document> viewerDocuments = [];
+
+  Future<Map<String, dynamic>> getSharedDocuments(String userId) async {
+    print('Getting shared documents for user $userId');
+    final response = await http.get(
+      Uri.parse('http://localhost:8080/document/user/shared'),
+      headers: <String, String>{
+        'userId': userId,
+      },
+    );
+    print('response of get shared documents${response.body}');
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to load shared documents');
+    }
+  }
+
+  void loadSharedDocuments() async {
+    try {
+      Map<String, dynamic> sharedDocuments =
+          await getSharedDocuments(widget.userId);
+      setState(() {
+        for (var doc in sharedDocuments['editorDocuments']) {
+          editorDocuments.add(Document(
+              id: doc['id'],
+              name: doc['name'],
+              owner: doc['ownerId'],
+              isOwnedByUser: false));
+        }
+        for (var doc in sharedDocuments['viewerDocuments']) {
+          viewerDocuments.add(Document(
+              id: doc['id'],
+              name: doc['name'],
+              owner: doc['ownerId'],
+              isOwnedByUser: false));
+        }
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadSharedDocuments();
+  }
+
   @override
   Widget build(BuildContext context) {
+    List<Document> combinedDocuments = [...editorDocuments, ...viewerDocuments];
+
     return Expanded(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
@@ -35,9 +94,9 @@ class _SharedDocumentsState extends State<SharedDocuments> {
             child: ListView.builder(
               shrinkWrap: true,
               physics: NeverScrollableScrollPhysics(),
-              itemCount: widget.sharedDocuments.length,
+              itemCount: combinedDocuments.length,
               itemBuilder: (context, index) {
-                final document = widget.sharedDocuments[index];
+                final document = combinedDocuments[index];
                 return Container(
                   margin: const EdgeInsets.only(bottom: 10),
                   decoration: BoxDecoration(
@@ -47,7 +106,25 @@ class _SharedDocumentsState extends State<SharedDocuments> {
                   child: ListTile(
                     title: Text(document.name),
                     onTap: () {
-                      // TODO: Handle opening the document
+                      if (editorDocuments.contains(document)) {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => TextEdit(
+                              id: widget.userId,
+                            ),
+                          ),
+                        );
+                      } else if (viewerDocuments.contains(document)) {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ViewOnly(
+                              id: widget.userId,
+                            ),
+                          ),
+                        );
+                      }
                     },
                     trailing: PopupMenuButton<String>(
                       itemBuilder: (context) => widget.isEditor
@@ -68,7 +145,10 @@ class _SharedDocumentsState extends State<SharedDocuments> {
                           showDialog(
                             context: context,
                             builder: (BuildContext context) {
-                              return ShareDocumentDialog();
+                              return ShareDocumentDialog(
+                                userId: widget.userId,
+                                documentName: document.name,
+                              );
                             },
                           );
                         }
